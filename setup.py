e6e2303b5bad0c745e7a7fb62b06f4d981fea301
Dyaln Uys <dylan.uys@gmail.com>
2024-10-04 20:56:49 +0000

    setup.py and gif

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sim',  
+    version='0.1.0',  
+    description='Subnet simulator 9000',
+    author='dubm',
+    author_email='dylan@bitmindlabs.ai',
+    packages=find_packages(),
+    install_requires=[],
+)
