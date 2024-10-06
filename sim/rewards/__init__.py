db98459330908743f1c8bffc97f6a29a449dabbc
Dyaln Uys <dylan.uys@gmail.com>
2024-10-06 01:04:56 +0000

    adding glicko Rewards subclass

--- sim/rewards/__init__.py
+++ sim/rewards/__init__.py
@@ -2,3 +2,4 @@ from .reward_registry import REWARD_REGISTRY
 from .base_reward import Reward
 from .weighted_history import WeightedHistoryReward
 from .binary import BinaryReward
+from .glicko import GlickoReward
